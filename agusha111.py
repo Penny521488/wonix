da15ee4e40b72c6f5c4a35a025b9fbc22dbe97da
LEGION <golu_la67@student.itstep.org>
2024-12-03 15:54:58 +0200

    Added age-check future

--- agusha111.py
+++ agusha111.py
@@ -2,3 +2,9 @@ print("Hello World")
 
 name = input("Як тебе звуть?")
 print(f"Привіт , {name}!")
+
+age=int(input("Скільки тобі років?"))
+if age <=18 :
+    print("малолєтка")
+if age >=18 :
+    print("ото ти дєд")
